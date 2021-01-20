fbba47b2ede0b8ffb11eb63466b0849415f81fda
e1b18034 <e1b18034@st.oit.ac.jp>
2021-01-20 13:37:17 +0900

    滞在記録機能及びランキング出力機能の実装
    各ポイント訪問回数と滞在時間の記録
    平均滞在時間をもとにしたランキング出力

---
+++ Assets/Script/StayTimer/StopWatch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopWatch : MonoBehaviour
+{
+    float sumTime;
+    bool stop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.sumTime = 0;
+        this.stop = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!this.stop) {
+            this.sumTime += Time.deltaTime;
+        }
+    }
+
+    /**
+     * * Stopのtrue/false反転
+    */
+    public void Alternate() {
+        this.stop = !this.stop;
+    }
+
+    /**
+     * * Stopであるか否か
+    */
+    public bool IsStop() {
+        return this.stop;
+    }
+
+    /**
+     * * 通常リセット
+    */
+    public void ResetStopWatch() {
+        this.stop = true;
+        this.sumTime = 0;
+    }
+
+    /**
+     * * 初期値指定リセット
+    */
+    public void ResetStopWatch(float time) {
+        this.stop = true;
+        this.sumTime = time;
+    }
+
+    public float GetTime() {
+        return this.sumTime;
+    }
+}
